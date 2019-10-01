7e745c064548c914e456626b854088e98c1cc400
Maya Bowman <mayashady1@gmail.com>
2019-10-01 12:28:27 -0400

    worked through solution

--- src/components/app.js
+++ src/components/app.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import IncrementButton from './increment-button';
+import React from 'react'
+import IncrementButton from './increment-button'
 
-export default class App extends React.Component {
+class App extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             count: 0
-        };
+        }
+    }
+
+
+    increment() {
+        this.setState({
+            count: this.state.count + 1
+        })
     }
 
     render() {
         return (
-            <div className="parent">
-                <div className="count">
+            <div className='parent'>
+                <div className='count'>
                     Number of clicks: {this.state.count}
                 </div>
-                <IncrementButton />
+                <IncrementButton incrementClick={() => this.increment()} />
             </div>
-        );
+        )
     }
 }
+
+export default App
--- src/components/increment-button.js
+++ src/components/increment-button.js
@@ -1,27 +1,5 @@
 import React from 'react'
 
-class IncrementButton extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            number: 0
-        }
-
-        this.clickHandler = this.clickHandler.bind(this)
-    }
-
-    clickHandler() {
-        this.setState({
-            number: this.state.number + 1
-        })
-    }
-
-    render() {
-        return (
-            <button onClick={this.clickHandler}>Increment</button>
-        )
-    }
-    
+export default function IncrementButton(props) {
+    return <button onClick={() => props.incrementClick()}>Increment</button>;
 }
-
-export default IncrementButton
