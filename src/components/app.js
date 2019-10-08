be96541a8143aa6474d59deaa4d070ec8e086881
Maya Bowman <mayashady1@gmail.com>
2019-10-08 04:37:19 -0400

    practice and variations

--- src/components/app.js
+++ src/components/app.js
@@ -2,14 +2,15 @@ import React from 'react'
 import IncrementButton from './increment-button'
 
 class App extends React.Component {
-  constructor(props) {
-      super(props)
-      this.state = {
-          count: 0
-      }
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+    this.incrementClick = this.incrementClick.bind(this)
   }
 
-  handleClick() {
+  incrementClick() {
     this.setState({
       count: this.state.count + 1
     })
@@ -17,14 +18,74 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className='parent'>
-          <div className='count'>
-              Number of clicks: {this.state.count}
-          </div>
-          <IncrementButton incrementClick={() => this.handleClick()}/>
+      <div>
+        <p>Number of Clicks: {this.state.count}</p>
+        <IncrementButton incrementClick={this.incrementClick}/>
       </div>
     )
   }
-}
-
+ }
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import IncrementButton from './increment-button'
+
+// class App extends React.Component {
+//   constructor(props) {
+//       super(props)
+//       this.state = {
+//           count: 0
+//       }
+//   }
+
+//   handleClick() {
+//     this.setState({
+//       count: this.state.count + 1
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div className='parent'>
+//           <div className='count'>
+//               Number of clicks: {this.state.count}
+//           </div>
+//           <IncrementButton incrementClick={() => this.handleClick()}/>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
--- src/components/increment-button.js
+++ src/components/increment-button.js
@@ -1,5 +1,51 @@
 import React from 'react'
 
-export default function IncrementButton(props) {
-  return <button onClick={() => props.incrementClick()}>Increment</button>;
+function IncrementButton(props) {
+  return (
+    <button onClick={props.incrementClick}>Increment</button>
+  )
 }
+
+export default IncrementButton
+
+{/* <button onClick={() => props.incrementClick()}>Increment</button> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+
+// export default function IncrementButton(props) {
+//   return <button onClick={() => props.incrementClick()}>Increment</button>;
+// }
